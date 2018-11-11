e46f68b949cb36245faddcb8497d7045a44f80ba
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2018-11-11 16:41:30 +0000

    Stuff and Things

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+import discord, time
 from discord.ext import commands
 from auth import bot_token
 import datetime
@@ -7,7 +7,27 @@ bot = commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_typing(channel, user, time):
-    print(f'{user.name}#{user.discriminator} is typing in #{channel.name} - {channel.guild.name} at {time}')
+    # Will have to make a register so that people who are being watched for a message cant trigger this.
+
+    startTime = time.time() # Using time module for length so that theyre in the same format.
+    print(f'{user.display_name}#{user.discriminator} is typing in #{channel.name} - {channel.guild.name} at {time}')
+
+    def check(m):
+        return m.channel == channel and m.author == user
+    try:
+        msg = await client.wait_for("message", check=check, timeout=20.0)
+    except asyncio.TimeoutError:
+        print("{0.display_name}#{0.discriminator} didn\'t send a message".format(user))
+        return
+
+    endTime = time.time()
+    durationTime = endTime - startTime
+
+    # Check for nitro with author.nitro, if its false we can iterate through guild.emoji to see if theyve used any emotes in their message
+    # If they do have nitro, we'll have to use a regex to see if theyve sent an emote
+    # if bool(re.search("<:(.*):(.*)>", msg.content)) or bool(re.search("<a:(.*):(.*)>", msg.content)):
+
+    print("{0.author.display_name} took {1} seconds to send the message \"{0.content}\"".format(msg, durationTime))
 
 @bot.event
 async def on_message(message):
