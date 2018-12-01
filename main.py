5e55d968f98808f3ba584da4b0054d34d1027eb3
Aran Wittert <aran@jobadder.com>
2018-12-01 12:40:05 +1100

    change user.display_name to user.name

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ async def on_typing(channel, user, time):
     # Will have to make a register so that people who are being watched for a message cant trigger this.
     # We can set their watched status to True here (1/3)
     startTime = time.time() # Using time module for length so that theyre in the same format.
-    print(f'{user.display_name}#{user.discriminator} is typing in #{channel.name} - {channel.guild.name} at {time}')
+    print(f'{user.name}#{user.discriminator} is typing in #{channel.name} - {channel.guild.name} at {time}')
 
     def check(m):
         return m.channel == channel and m.author == user
     try:
         msg = await client.wait_for("message", check=check, timeout=20.0)
     except asyncio.TimeoutError:
-        print("{0.display_name}#{0.discriminator} didn\'t send a message".format(user))
+        print("{0.name}#{0.discriminator} didn\'t send a message".format(user))
         # Set their watched status to false when we know they didnt send a message (3/3)
         return
 
@@ -28,7 +28,7 @@ async def on_typing(channel, user, time):
         formattedMsg = re.sub("<:(.*):(.*)>", "",formattedMsg)
         formattedMsg = re.sub("<a:(.*):(.*)>", "",formattedMsg)
 
-    print("{0.author.display_name}#{0.author.discriminator} took {1} seconds to send the message \"{2}\"".format(msg, durationTime, formattedMsg))
+    print("{0.author.name}#{0.author.discriminator} took {1} seconds to send the message \"{2}\"".format(msg, durationTime, formattedMsg))
 
     #Use two JSON formatted files, one to store the final average for each user and one to store all their previous scores
     #the one that stores their scores would be used to make the average, and the final average file would be for ease of reading their latest
